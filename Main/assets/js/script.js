0f7257f83a6b96dc0333ea83e8a89b184ad005ce
Lavina91 <65462918+Lavina91@users.noreply.github.com>
2021-03-11 22:00:31 -0700

    added variables needed to js file

---
+++ Main/assets/js/script.js
@@ -0,0 +1,10 @@
+const allWords = ['javascript', 'array', 'object', 'condition', 'boolean'];
+let chosenWord = '';
+
+let numOfBlanks = 0;
+let wins = 0;
+let losses = 0;
+
+let isWin = false
+
+let timerCount;
